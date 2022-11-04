fa3d0d31f32c382bd92bddc47935c6c7cc243d57
Gianmarco Psotka <gianmarco@umail.ucsb.edu>
2022-11-03 17:50:31 -0700

    copied backend files for MenuItem from team02

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuRepository.java
@@ -0,0 +1,16 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenu;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UCSBDiningCommonsMenuRepository extends CrudRepository<UCSBDiningCommonsMenu, Long> {
+  Iterable<UCSBDiningCommonsMenu> findAllByDiningCommonsCode(String diningCommonsCode);
+}
+
+
+
+
