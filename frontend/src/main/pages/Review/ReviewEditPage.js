73c267ea0836d2091e5aac13ed461fd48ab165f8
Ethan Lee <elee1819@gmail.com>
2022-11-08 17:26:52 -0800

    Added Placeholder for Review Pages and the test for them. Also recopied over controller test from team02 correctly for MenuItemReview.

---
+++ frontend/src/main/pages/Review/ReviewEditPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function ReviewEditPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Todos</h1>
+        <p>
+          This is where the Review edit page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
