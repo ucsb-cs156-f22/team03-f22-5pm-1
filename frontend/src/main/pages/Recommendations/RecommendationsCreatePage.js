8ee6610982dfae7902b8d2edeca33dced0eb378d
isaac <ihoffman@ucsb.edu>
2022-11-08 18:13:05 -0800

    ih - add placeholder for createpage + tests

---
+++ frontend/src/main/pages/Recommendations/RecommendationsCreatePage.js
@@ -0,0 +1,17 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
+export default function RecommendationsCreatePage() {
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+            <h1>Create New Recommendation Request</h1>
+            
+            <p>Placeholder for form to create Recommendation request</p>    
+            </div>
+        </BasicLayout>
+    )
+}
