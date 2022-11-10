c2c89217c3aa44bebf34608a131408fdfbe96747
Christian Schwenger <drikdrok02@gmail.com>
2022-11-09 17:36:18 -0800

    OrganizationTable on Storybook

---
+++ frontend/src/fixtures/organizationFixtures.js
@@ -0,0 +1,31 @@
+const organizationFixtures = {
+    oneOrganizations: {
+        "orgCode": "test1",
+        "orgTranslationShort": "test1",
+        "orgTranslation": "test1",
+        "inactive": false,
+    },
+    threeOrganizations: [
+        {
+            "orgCode": "test1",
+            "orgTranslationShort": "test1",
+            "orgTranslation": "test1",
+            "inactive": false,
+        },
+        {
+            "orgCode": "test2",
+            "orgTranslationShort": "test2",
+            "orgTranslation": "test2",
+            "inactive": false,
+        },
+        {
+            "orgCode": "test3",
+            "orgTranslationShort": "test3",
+            "orgTranslation": "test3",
+            "inactive": false,
+        } 
+    ]
+};
+
+
+export { organizationFixtures };
