750f3f293f7612c347ac7fcc0961d8e028c837ef
isaac <ihoffman@ucsb.edu>
2022-11-10 18:05:09 -0800

    ih - add RecommendationTable and tests

---
+++ frontend/src/main/components/Recommendation/RecommendationTable.js
@@ -0,0 +1,62 @@
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+// import { useBackendMutation } from "main/utils/useBackend";
+// import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function RecommendationTable({ recommendation, currentUser }) {
+    const navigate = useNavigate();
+
+    const editCallback = (cell) => {
+        navigate(`/recommendation/edit/${cell.row.values.id}`)
+    }
+
+    // TODO: add delete callback
+
+    const columns = [
+        {
+            Header: 'Id',
+            accessor: 'id', 
+        },
+        {
+            Header: 'Requester Email',
+            accessor: 'requesterEmail',
+        },
+        {
+            Header: 'Professor Email',
+            accessor: 'professorEmail',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Date Requested',
+            accessor: 'dateRequested',
+        },
+        {
+            Header: 'Date Needed',
+            accessor: 'dateNeeded',
+        },
+        {
+            Header: 'Done?',
+            id: 'done', // needed for tests
+            accessor: (row, _rowIndex) => String(row.done) // hack needed for boolean values to show up
+        }
+    ];
+
+    const testid = "RecommendationTable";
+
+    const columnsIfAdmin = [
+        ...columns,
+        ButtonColumn("Edit", "primary", editCallback, testid),
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={recommendation}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
