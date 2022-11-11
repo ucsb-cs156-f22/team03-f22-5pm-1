9a3e8beb987b5951c58435a116680a9b9066d6b9
Ethan Lee <elee1819@gmail.com>
2022-11-11 11:31:18 -0800

    Review create and test

--- frontend/src/main/pages/Review/ReviewCreatePage.js
+++ frontend/src/main/pages/Review/ReviewCreatePage.js
@@ -1,13 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import ReviewForm from "main/components/Review/ReviewForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 export default function ReviewCreatePage() {
+
+  const objectToAxiosParams = (review) => ({
+    url: "/api/reviews/post",
+    method: "POST",
+    params: {
+      itemID: review.itemID,
+      reviewerEmail: review.reviewerEmail,
+      stars: review.stars,
+      dateReviewed: review.dateReviewed,
+      comments: review.comments
+    }
+  });
+
+  const onSuccess = (review) => {
+    toast(`New review Created - id: ${review.id}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/reviews/all"]
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/reviews/list" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>ReviewCreatePage</h1>
-        <p>
-          This is where the Review create page will go
-        </p>
+        <h1>Create New Review</h1>
+
+        <ReviewForm submitAction={onSubmit} />
+
       </div>
     </BasicLayout>
   )
