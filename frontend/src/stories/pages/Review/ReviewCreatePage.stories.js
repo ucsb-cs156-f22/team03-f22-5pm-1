2d75a7841c345c1d030a8f45117e68eca1ea4c2b
Ethan Lee <elee1819@gmail.com>
2022-11-12 17:17:52 -0800

    Added Review pages under stories directory

---
+++ frontend/src/stories/pages/Review/ReviewCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import ReviewCreatePage from "main/pages/Review/ReviewCreatePage";
+
+export default {
+    title: 'pages/Review/ReviewCreatePage',
+    component: ReviewCreatePage
+};
+
+const Template = () => <ReviewCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
---
+++ frontend/src/stories/pages/Review/ReviewEditPage.stories.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import ReviewEditPage from "main/pages/Review/ReviewEditPage";
+
+export default {
+    title: 'pages/Review/ReviewEditPage',
+    component: ReviewEditPage
+};
+
+const Template = () => <ReviewEditPage />;
+
+export const Default = Template.bind({});
+
+
+
